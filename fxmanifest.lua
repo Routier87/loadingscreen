6d0f8c0656f8580e42c212a1ae226088cdf91c4d
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-02-21 18:49:06 +0100

    Add fxmanifest.lua for resource configuration

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+
+files {
+  'index.html',
+  'style.css',
+  'script.js',
+  'images/background.png',
+  'images/logo.png',
+  'music/music.mp3'
+}
